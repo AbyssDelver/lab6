7a12d161b59aa2db4ceb7dd19af02ed421777652
AbyssDelver <muffinarmageddon@gmail.com>
2023-04-18 16:54:09 +0200

    modified main to input and output from file

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include "statistics.hpp"
 #include <random>
 #include <iostream> 
+#include <fstream>
 
 int main(){
-    const int N = 100;
     pf::Sample data{};
-    std::random_device r;
-    std::default_random_engine eng(r());
-    std::normal_distribution<double> dist;
-    for(int i = 0; i < N; ++i){
-        data.add(dist(eng));
+    //std::random_device r;
+    //std::default_random_engine eng(r());
+
+    std::ifstream in_file("data.txt");
+    std::ofstream out_file{"./results.txt"};
+    double x;
+    while (in_file >>  x){
+        data.add(x);
     }
+
+    std::normal_distribution<double> dist;
     const auto statistics{data.statistics()};
-    std::cout << "- mean: " << statistics.mean << "\n" << "- sigma: " << statistics.sigma << "\n";
+    out_file << "- mean: " << statistics.mean << "\n" << "- sigma: " << statistics.sigma << "\n";
 }
